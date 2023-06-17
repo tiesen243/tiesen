867feecd6d9df801716fc2f7fb13d3ad545dbd68
Tiesen <ttien56906@gmail.com>
2023-06-17 17:56:48 +0700

    Github Graphql

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import parse from 'html-react-parser'
 
+import GET_REPOSITORY from '../GraphQL/Query'
 import me from '../data/personal'
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_REPOSITORY, {
+    variables: {
+      username: 'tiesen243',
+      repository: 'tiesen',
+    },
+  })
+  const posts: any[] = data?.repository?.discussions?.nodes
   return (
     <div className="container mr-auto ml-auto mb-10">
       <div>
@@ -27,7 +37,7 @@ const Home = () => {
         </p>
       </div>
       <div className="flex flex-col lg:flex-row gap-3 mx-2 -mt-6">
-        <div className="island w-full lg:w-2/5 sm:text-2xl lg:text-xl font-medium pl-4 pb-4">
+        <div className="island w-full h-fit lg:w-2/5 sm:text-2xl lg:text-xl font-medium pl-4 pb-4">
           <h2 className="island-text pb-4">About me</h2>
           <p>Date of birth: {me.DoB}</p>
           <p>Place: {me.Place}</p>
@@ -39,23 +49,31 @@ const Home = () => {
         <div className="island w-full lg:w-3/5 mb-6">
           <h2 className="island-text mb-4">My post</h2>
           <div className="flex flex-col gap-5">
-            <div className="island w-full bg-[#3e4754]">
-              <h1 className="font-bold text-2xl text-center">dsadsad</h1>
-              <p>
-                Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-                reprehenderit enim labore culpa sint ad nisi Lorem pariatur
-                mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
-                officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip
-                amet voluptate voluptate dolor minim nulla est proident. Nostrud
-                officia pariatur ut officia. Sit irure elit esse ea nulla sunt
-                ex occaecat reprehenderit commodo officia dolor Lorem duis
-                laboris cupidatat officia voluptate. Culpa proident adipisicing
-                id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
-                Aliqua reprehenderit commodo ex non excepteur duis sunt velit
-                enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur
-                et est culpa et culpa duis.
-              </p>
-            </div>
+            {posts?.map((post: any, index: number): any => {
+              return (
+                <div key={index} className="island w-full">
+                  <div className="flex flex-row">
+                    <img
+                      src={post.author.avatarUrl}
+                      alt="Author avatar"
+                      className="w-16 rounded-full shadow-lg shadow-black"
+                    ></img>
+                    <div className="flex flex-col pl-4">
+                      <h3 className="capitalize text-2xl font-bold">
+                        {post.author.login}
+                      </h3>
+                      <p>{post.createdAt}</p>
+                    </div>
+                  </div>
+                  <h2 className="text-3xl font-bold text-center mb-4">
+                    {post.title}
+                  </h2>
+                  <div className="island bg-[#242526] w-full">
+                    <p>{parse(post.bodyHTML)}</p>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
