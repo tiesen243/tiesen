14a0045fea88700acfd4c8817a565035357e7934
Tiesen <ttien56906@gmail.com>
2023-06-17 10:53:01 +0700

    add contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
+import contact from '../data/contact'
+
 function Contact() {
   return (
     <div className="container flex flex-row col-2 py-2 gap-5 mr-auto ml-auto">
       <div className="island">
         <h3 className="island-text">Contact</h3>
+        <ul>
+          <li>
+            <i className="fa-solid fa-envelope"></i>
+            Email: {contact.email}
+          </li>
+          <li>
+            <i className="fa-solid fa-phone"></i> Phone: {contact.phone}
+          </li>
+          <li>
+            <i className="fa-brands fa-linkedin"></i>
+            LinkedIn:
+            <a href={contact.linkedin}>Tran Tien</a>
+          </li>
+          <li>
+            Facebook:
+            <a target="_blank" href={contact.facebook}>
+              Tran Tien
+            </a>
+          </li>
+          <li>
+            Twitter:
+            <a target="_blank" href={contact.twitter}>
+              Tran Tien
+            </a>
+          </li>
+        </ul>
       </div>
       <div className="island">
         <h3 className="island-text">Social</h3>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,6 @@
 import React from 'react'
-import { Person } from '../interface'
 
-const me: Person = {
-  name: 'Trần Tiến',
-  DoB: '22/06/2004',
-  Place: 'Ho Chi Minh City, Viet Nam',
-  avatar: require('../assets/images/1640249997536.jpg'),
-  banner: require('../assets/images/bg_4x.png'),
-  college: 'Industrial University of Ho Chi Minh City',
-  favourite: ['Coding', 'Reading', 'Music', 'Game'],
-  languages: ['Vietnamese', 'English'],
-  skills: ['python', 'javascript', 'typescript', 'C++'],
-}
+import me from '../data/personal'
 
 const Home = () => {
   return (
@@ -40,6 +29,7 @@ const Home = () => {
         </div>
         <div className="island w-3/5">
           <h2 className="island-text">My post</h2>
+          <div></div>
         </div>
       </div>
     </div>
