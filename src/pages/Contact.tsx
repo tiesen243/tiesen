50433a9021f16c39a7b0ab93085d874be649623f
Tiesen <ttien56906@gmail.com>
2023-06-19 11:20:52 +0700

    add facebook comment plugin

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -81,6 +81,17 @@ const Contact: React.FC = () => {
         </div>
         <div className="island w-full lg:w-[49%]">
           <h3 className="island-text">Comment</h3>
+          <div className="flex justify-center bg-gray-50 rounded-lg shadow mx-auto my-4 p-2 w-11/12">
+            <div
+              className="fb-comments"
+              data-href="https://tiesen.vercel.app/contact"
+              data-width="600"
+              data-mobile="auto_detect"
+              data-colorscheme="dark"
+              data-numposts="5"
+              data-lazy="true"
+            ></div>
+          </div>
         </div>
       </div>
     </div>
