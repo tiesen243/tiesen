89f281e1d1c68967071ed766e886b0430f8c787d
Tiesen <ttien56906@gmail.com>
2023-06-21 22:35:24 +0700

    Reponsive

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className="flex flex-col gap-5 items-center justify-center h-screen -mt-16 select-none">
       <h1 className="text-center text-9xl font-black">404</h1>
       <p className="text-3xl font-semibold">Page not found 😅</p>
-      <p className="text-2xl font-medium">
+      <p className="sm:text-base md:text-xl lg:text-2xl font-medium break-words">
         The page you are looking for does not exist or has been removed. 😭
       </p>
       <Link to="/" className="font-medium text-lg uppercase">
